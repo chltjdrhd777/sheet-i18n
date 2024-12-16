8ef2095e6fad44650d8179ce0f5408c06bd0ee2e
devAnderson <chltjdrhd777@naver.com>
2024-12-16 11:30:29 +0900

    fix: eslint typescript 전역으로 설정되도록 수정

---
+++ eslint.config.cjs
@@ -0,0 +1,22 @@
+const js = require("@eslint/js");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+    },
+  },
+];
