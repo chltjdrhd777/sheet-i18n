53d3e0c6aced647e3b47063cd23bf134345e73a5
devAnderson <chltjdrhd777@naver.com>
2024-12-16 14:05:51 +0900

    feat: add lint-staged pre commit script

---
+++ lint-staged.config.mjs
@@ -0,0 +1,29 @@
+import { relative } from 'path';
+import { ESLint } from 'eslint';
+
+const getIgnoredFiles = async (files) => {
+  const cwd = process.cwd();
+  const eslint = new ESLint();
+  const relativePaths = files.map((file) => relative(cwd, file));
+  const isIgnored = await Promise.all(
+    relativePaths.map((file) => eslint.isPathIgnored(file))
+  );
+  const filteredFiles = files.filter((_, i) => !isIgnored[i]);
+
+  return filteredFiles.join(' ');
+};
+
+export default {
+  '**/*.{js,ts,jsx,tsx}': async (files) => {
+    const filesToLint = await getIgnoredFiles(files);
+
+    return [`eslint --max-warnings=0 --fix ${filesToLint}`];
+  },
+  '**/*.css': async (files) => {
+    const filesToLint = await getIgnoredFiles(files);
+
+    return [
+      `prettier --config .prettierrc.json --ignore-path --write ${filesToLint}`,
+    ];
+  },
+};
